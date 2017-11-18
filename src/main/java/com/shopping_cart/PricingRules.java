8df5c645674f443ef63f776df135a5fddee7a00a
Ideen Faraji <ideen.faraji@servian.com.au>
2017-11-19 00:53:17 +1100

    Pricing rules - implementing empty or null list of items

--- src/main/java/com/shopping_cart/PricingRules.java
+++ src/main/java/com/shopping_cart/PricingRules.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class PricingRules {
 	public List<Item> apply(List<Item> items){
+		if (items == null || items.isEmpty())
+			throw new RuntimeException("Invalid number of items");
 		return null;
 	}
 }
