cfbb2c898699246c1479b587d14c95c19ae4dcbe
Ideen Faraji <ideen.faraji@servian.com.au>
2017-11-19 01:24:54 +1100

    Pricing rules - implementing when no discount is applicable

--- src/main/java/com/shopping_cart/PricingRules.java
+++ src/main/java/com/shopping_cart/PricingRules.java
@@ -6,6 +6,6 @@ public class PricingRules {
 	public ArrayList<Item> apply(ArrayList<Item> items){
 		if (items == null || items.isEmpty())
 			throw new RuntimeException("Invalid number of items");
-		return null;
+		return items;
 	}
 }
